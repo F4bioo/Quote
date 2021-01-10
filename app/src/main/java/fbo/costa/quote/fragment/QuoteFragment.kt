3c4280780f89d4a5d6948fbfcc24b845e5978e13
F4bioo <costa.fbo@gmail.com>
2021-01-10 16:59:58 +0000

    05 insert quote in database

--- app/src/main/java/fbo/costa/quote/fragment/QuoteFragment.kt
+++ app/src/main/java/fbo/costa/quote/fragment/QuoteFragment.kt
@@ -1,22 +1,94 @@
 package fbo.costa.quote.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import fbo.costa.quote.R
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.snackbar.Snackbar
+import fbo.costa.quote.data.AppDatabase
+import fbo.costa.quote.data.QuoteDao
+import fbo.costa.quote.databinding.QuoteFragmentBinding
+import fbo.costa.quote.extension.hideKeyboard
+import fbo.costa.quote.repository.DatabaseDataSource
+import fbo.costa.quote.repository.QuoteRepository
 import fbo.costa.quote.viewmodel.QuoteViewModel
 
 class QuoteFragment : Fragment() {
 
-    private lateinit var viewModel: QuoteViewModel
+    private var _binding: QuoteFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: QuoteViewModel by viewModels {
+        object : ViewModelProvider.Factory {
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                val quoteDao: QuoteDao =
+                    AppDatabase.getInstance(requireContext()).quoteDao
+
+                val repository: QuoteRepository = DatabaseDataSource(quoteDao)
+                return QuoteViewModel(repository) as T
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.quote_fragment, container, false)
+    ): View {
+        _binding = QuoteFragmentBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        observeEvents()
+        setListeners()
+    }
+
+    private fun observeEvents() {
+        viewModel.quoteStateEventData.observe(viewLifecycleOwner) { quoteState ->
+            when (quoteState) {
+                is QuoteViewModel.QuoteState.Inserted -> {
+                    clearFields()
+                    hideKeyboard()
+                    requireView().requestFocus()
+                }
+            }
+            viewModel.messageEventData.observe(viewLifecycleOwner) { stringResId ->
+                Snackbar.make(requireView(), stringResId, Snackbar.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun clearFields() {
+        binding.inputQuote.text?.clear()
+        binding.inputAuthor.text?.clear()
+    }
+
+    private fun hideKeyboard() {
+        val parentActivity = requireActivity()
+        if (parentActivity is AppCompatActivity) {
+            parentActivity.hideKeyboard()
+        }
+    }
+
+    private fun setListeners() {
+        binding.buttonAdd.setOnClickListener {
+            val name = binding.inputQuote.text.toString()
+            val email = binding.inputAuthor.text.toString()
+
+            viewModel.addQuote(name, email)
+        }
+    }
 }
--- app/src/main/java/fbo/costa/quote/viewmodel/QuoteViewModel.kt
+++ app/src/main/java/fbo/costa/quote/viewmodel/QuoteViewModel.kt
@@ -1,7 +1,44 @@
 package fbo.costa.quote.viewmodel
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import fbo.costa.quote.R
+import fbo.costa.quote.repository.QuoteRepository
+import kotlinx.coroutines.launch
 
-class QuoteViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class QuoteViewModel(
+    private val repository: QuoteRepository
+) : ViewModel() {
+
+    private val _quoteStateEventData = MutableLiveData<QuoteState>()
+    val quoteStateEventData: LiveData<QuoteState>
+        get() = _quoteStateEventData
+
+    private val _messageEventData = MutableLiveData<Int>()
+    val messageEventData: LiveData<Int>
+        get() = _messageEventData
+
+    fun addQuote(name: String, email: String) = viewModelScope.launch {
+        try {
+            val id = repository.insert(name, email)
+            if (id > 0) {
+                _quoteStateEventData.value = QuoteState.Inserted
+                _messageEventData.value = R.string.quote_inserted_successfully
+            }
+        } catch (ex: Exception) {
+            _messageEventData.value = R.string.quote_error_to_insert
+            Log.e(TAG, ex.toString())
+        }
+    }
+
+    sealed class QuoteState {
+        object Inserted : QuoteState()
+    }
+
+    companion object {
+        private val TAG = QuoteViewModel::class.java.simpleName
+    }
 }
