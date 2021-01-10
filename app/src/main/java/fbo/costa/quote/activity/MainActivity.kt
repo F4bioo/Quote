0fe5b4cb8f2fd8b44909cc461bef013b99702653
F4bioo <costa.fbo@gmail.com>
2021-01-10 16:07:58 +0000

    01 starting project

--- app/src/main/java/fbo/costa/quote/activity/MainActivity.kt
+++ app/src/main/java/fbo/costa/quote/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package fbo.costa.quote
+package fbo.costa.quote.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fbo.costa.quote.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
