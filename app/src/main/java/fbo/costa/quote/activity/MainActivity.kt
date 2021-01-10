dbbbe426304c4b793877528897b150e92a3bce17
F4bioo <costa.fbo@gmail.com>
2021-01-10 16:49:22 +0000

    03 adding toolbar

--- app/src/main/java/fbo/costa/quote/activity/MainActivity.kt
+++ app/src/main/java/fbo/costa/quote/activity/MainActivity.kt
@@ -1,12 +1,26 @@
 package fbo.costa.quote.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import fbo.costa.quote.R
+import fbo.costa.quote.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 }
